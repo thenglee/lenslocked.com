b11428aadaef4559e81b0178ab4056891b1bf02d
Tan Theng Lee <thengleetan@gmail.com>
2021-05-18 17:06:35 +0800

    Add contact page

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, _ *http.Request) {
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>\n")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>\n")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:supprt@lenslocked.com\">support@lenslocked.com</a>")
+	}
 }
 
 func main() {
