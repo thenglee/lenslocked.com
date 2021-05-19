575dff84b18a144b18665d327ee4011552b2a71b
Tan Theng Lee <thengleetan@gmail.com>
2021-05-19 16:03:49 +0800

    Add custom 4040 page

--- main.go
+++ main.go
@@ -17,9 +17,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:supprt@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+func notfound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Page not found</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(notfound)
 	http.ListenAndServe(":3000", r) // use DefaultServeMux
 }
