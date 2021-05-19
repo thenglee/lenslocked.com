91963f063bcb29c98886bb37aaedba2ef4d71b7e
Tan Theng Lee <thengleetan@gmail.com>
2021-05-19 16:08:35 +0800

    Try out another router

--- main.go
+++ main.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>\n")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:supprt@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	http.ListenAndServe(":3000", r) // use DefaultServeMux
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	http.ListenAndServe(":3000", router)
 }
