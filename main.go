141825fb768a23caacb8c92ff12431e0e5dfb837
Tan Theng Lee <thengleetan@gmail.com>
2021-05-20 13:05:29 +0800

    Add 404 page

--- main.go
+++ main.go
@@ -17,9 +17,16 @@ func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:supprt@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, but we couldn't find the page you were looking for</h1>")
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", home)
 	router.GET("/contact", contact)
+	router.NotFound = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", router)
 }
