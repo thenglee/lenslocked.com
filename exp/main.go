f85c251fe81aa45c922ba9c118f61912bae7459e
Tan Theng Lee <thengleetan@gmail.com>
2021-05-21 14:21:47 +0800

    Ex3 Solution: render with nested structures

--- exp/main.go
+++ exp/main.go
@@ -5,12 +5,13 @@ import (
 	"os"
 )
 
+type Dog struct {
+	Name string
+}
+
 type User struct {
-	Name  string
-	Int   int
-	Float float64
+	Dog   Dog
 	Slice []string
-	Map   map[string]string
 }
 
 func main() {
@@ -20,14 +21,10 @@ func main() {
 	}
 
 	data := User{
-		Name:  "John Smith",
-		Int:   123,
-		Float: 3.14,
-		Slice: []string{"a", "b", "c"},
-		Map: map[string]string{
-			"key1": "value1",
-			"key2": "value2",
+		Dog: Dog{
+			Name: "Morty",
 		},
+		Slice: []string{"a", "b", "c"},
 	}
 
 	err = t.Execute(os.Stdout, data)
